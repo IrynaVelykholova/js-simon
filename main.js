6d41db4fc5b022275da8f4b93fbc20388a0d2f42
IrynaVelykholova <irynavely93@gmail.com>
2023-07-13 18:12:12 +0200

    countdown finito

--- main.js
+++ main.js
@@ -15,3 +15,31 @@ function Orologio() {
     document.getElementById("orologio").innerHTML= ora+ ":" + min+ ":" + sec;
 }
 
+
+//scelgo la data del countdown
+const countDownDate = new Date("July 14, 2023 09:30").getTime();
+
+// aggiorno ogni secondo
+const x = setInterval(function() {
+
+//prendo il tempo attuale
+const now = new Date().getTime();
+
+// variabile countdown e ora
+const distance = countDownDate - now;
+
+// tempo calcolato per giorno, ora, minuti e secondi
+const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+// inserisco in html
+document.getElementById("countdown").innerHTML = days + "giorni " + hours + "ore " + minutes + "minuti " + seconds + "secondi ";
+
+//se il countdown finisce allora scrivo un messaggio
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "TEMPO SCADUTO!";
+}
+}, 1000);
